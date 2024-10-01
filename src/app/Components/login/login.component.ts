d2040684e7b769d4a32c151856a06d3719bc4286
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-10-01 17:19:23 +0100

    LoginComponent controller finished

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { UserDTO } from '../../Models/user.dto';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  user: UserDTO = new UserDTO('','');
 
+  email: FormControl = new FormControl(this.user.email);
+  password: FormControl = new FormControl(this.user.password);
+  loginForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: this.email,
+      password: this.password
+    });
+  }
+
+  checkLogin(): void {
+    this.user.email = this.email.value;
+    this.user.password = this.password.value;
+
+    console.log(
+      'User: ',
+      this.user.email,
+      '\nPassword: ',
+      this.user.password
+    );
+  }
 }
