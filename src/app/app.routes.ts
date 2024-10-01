63d4cc0f911e0c13c50e96305f645a253f790ce1
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-10-01 11:01:44 +0100

    Home route now leads to LoginComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+// import { SignInComponent } from './Components/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:'',
+        component: LoginComponent
+    },
+    // {
+    //     path:'sign-in',
+    //     component: SignInComponent
+    // }
+];
