0fd3372e1e0cf56d3f497123a2e37f9d19222cc0
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-10-02 11:39:07 +0100

    Added new fields to UserDTO

--- src/app/Models/user.dto.ts
+++ src/app/Models/user.dto.ts
@@ -1,27 +1,27 @@
 export class UserDTO {
     email: string;
     password: string;
-    // name: string;
-    // surname1: string;
-    // surname2: string;
-    // alias: string;
-    // birthdate: Date;
+    name: string;
+    surname1: string;
+    surname2: string;
+    alias: string;
+    birthdate: Date;
 
     constructor(
         email: string,
         password: string,
-        // name: string,
-        // surname1:string,
-        // surname2: string,
-        // alias: string,
-        // birthdate: Date
+        name: string,
+        surname1:string,
+        surname2: string,
+        alias: string,
+        birthdate: Date
     ){
         this.email = email;
         this.password = password;
-        // this.name = name;
-        // this.surname1 = surname1;
-        // this.surname2 = surname2;
-        // this.alias = alias;
-        // this.birthdate = birthdate;
+        this.name = name;
+        this.surname1 = surname1;
+        this.surname2 = surname2;
+        this.alias = alias;
+        this.birthdate = birthdate;
     }
 }
